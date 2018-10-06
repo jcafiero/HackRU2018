fc543baae3275f1c88d490e718b7deb1e78626b2
Jennifer Cafiero <jcafiero@stevens.edu>
2018-10-06 15:37:09 -0400

    Add getPersonSkills function to index.js

--- index.js
+++ index.js
@@ -11,8 +11,71 @@ const options = {
     }
 };
 
+<<<<<<< Updated upstream
 let main = async () => {
 
 
     await fs.writeFile('report.json', JSON.stringify(report, null, 4));
 }
+=======
+let report = {};
+let jobReport = {};
+let companyIDs = [13]
+
+let main = async () => {
+    for (let id of companyIDs) {
+        let jobs = await getJobs(id);
+        jobReport[id] = (jobs);
+        // let persons = await getAllPeople(id);
+        // report['people'] = persons;
+    }
+    console.log(jobReport);
+    await getPersonSkills(13, 2049);
+
+
+    await fs.writeFile('report.json', JSON.stringify(jobReport, null, 4));
+}
+
+let getJobs = async (companyID) => {
+    options.url = `${companyID}/jobs`;
+    let jobsList = [];
+    req = await axios.request(options).catch((e) => {console.log(e)});
+    if (req) {
+        jobsList = req.data;
+    }
+    return { jobsList };
+};
+
+let getPersonSkills = async (companyID, personID) => {
+    let skills = [];
+    options.url = `${companyID}/people/${personID}`;
+    req = await axios.request(options).catch((e) => {console.log(e)});
+    if (req) {
+        education = req.data.education;
+        for (let school of education) {
+            skills.push(school.major);
+        }
+        skills = req.data.skills;
+        for (let skill of skills) {
+            if (skill.name) {
+                skills.push(skill.name);
+            }
+        }
+    }
+    return skills;
+}
+
+// let getAllPeople = async (companyID) => {
+//     options.url = `${companyID}/people`;
+//     let people = [];
+//     req = await axios.request(options).catch((e) => {console.log(e)});
+//     console.log(req);
+//     if (req) {
+//         // console.log(req.data);
+//         people = req.data.id;
+//     }
+//     return people;
+// }
+
+main().catch(e => {console.error(e)});
+>>>>>>> Stashed changes
