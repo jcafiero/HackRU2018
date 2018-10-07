a28bffabdc8fea70378abbbe97bdf0a0105e4e03
Jennifer Cafiero <jcafiero@stevens.edu>
2018-10-06 21:01:07 -0400

    Adds react-bootstrap and other fun stuff

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Button } from 'react-bootstrap';
+
 import './App.css';
 
+let btn1 = "";
+let btns = []
 class App extends Component {
+  // btn1 = document.getElementById('math-btn');
+  // btns = [btn1];
+  // getBtnId(btn) {
+  //     btn.addEventListener('click', () => {
+  //         console.log(this.id);
+  //     });
+  // }
+  // btns.foreach(getBtnId);
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload. This is a test
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h2 className="heading col-12">I want to study...</h2>
+        <div className="row">
+            <div className="col-3">&nbsp;</div>
+            <div className="body col-6">
+                <Button onClick={this.handleClick} id="math-btn" className="col-4 btn btn-primary button" bsSize="large">Math</Button>
+                <Button id="cs-btn" className="col-4 btn btn-primary button" bsSize="large">Computer Science</Button>
+                <Button id="science-btn" className="col-4 btn btn-primary button" bsSize="large">Science</Button>
+                <Button id="english-btn" className="col-4 btn btn-primary button" bsSize="large">English</Button>
+                <Button id="art-btn" className="col-4 btn btn-primary button" bsSize="large">Art</Button>
+                <Button id="spanish-btn"className="col-4 btn btn-primary button" bsSize="large">Spanish</Button>
+            </div>
+            <div className="col-3">&nbsp;</div>
+        </div>
       </div>
     );
   }
---
+++ app/src/Tile.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Tile extends Component {
+    render() {
+        return (
+            <div className="card">
+                <div className="card-title">
+                    Tile
+                </div>
+            </div>
+        )
+
+    }
+}
